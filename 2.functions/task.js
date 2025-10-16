c21f4f75cc5ec09eae532dd6b5731d674536c738
Svetlana Luchina <SvetlanaLuchina03@ya.ru>
2025-10-16 21:42:23 +0300

    Первое задание выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,23 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    if (element > max) {
+      max = element;
+    }
+    if (element < min) {
+      min = element;
+    }
+    sum += element;
+  }
+  let avg = Number((sum / arr.length).toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
+
 
 function summElementsWorker(...arr) {
 

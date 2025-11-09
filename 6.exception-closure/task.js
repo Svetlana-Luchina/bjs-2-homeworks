f38786e5f67b07c05b3871364f311c9c6a1d8dbf
Svetlana Luchina <SvetlanaLuchina03@ya.ru>
2025-11-09 17:05:05 +0300

    Оба задания готовы

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+  const paarseValue = Number.parseFloat(value);
+  if (isNaN(paarseValue)) {
+    throw new Error("Невалидное значение");
+  }
+  return paarseValue;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (Error) {
+    return Error;
+  }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}

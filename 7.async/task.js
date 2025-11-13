852a305855410ee5186132b8853cab08b03e9ad5
Svetlana Luchina <SvetlanaLuchina03@ya.ru>
2025-11-13 21:57:13 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,87 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (time === null || callback === undefined) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        let sameTime = false;
+        for (let i = 0; i < this.alarmCollection.length; i++) {
+            if (this.alarmCollection[i].time === time) {
+                sameTime = true;
+                break;
+            }
+        }
+
+        if (sameTime) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({
+            time: time,
+            callback: callback,
+            canCall: true
+        });
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        let now = new Date();
+
+        let hours = now.getHours();
+        if (hours < 10) {
+            hours = '0' + hours;
+        } else {
+            hours = hours.toString();
+        }
+
+        let minutes = now.getMinutes();
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        } else {
+            minutes = minutes.toString();
+        }
+        return hours + ':' + minutes;
+    }
+
+    start() {
+        if (this.intervalId !== null) {
+            return;
+        }
+
+        this.intervalId = setInterval(() => {
+            let currentTime = this.getCurrentFormattedTime();
+
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === currentTime && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        }, 1000);
+    }
+
+    stop() {
+        if (this.intervalId !== null) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => {
+            alarm.canCall = true;
+        });
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
